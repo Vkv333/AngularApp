2b7a1f8d4ac1ad592ff3da4ae569598275ef1110
Vkv333 <aha010h@gmail.com>
2025-05-16 00:07:39 +0530

    updated pjson yml server

--- AngularHON/server.js
+++ AngularHON/server.js
@@ -1,11 +1,16 @@
+// server.js
 const express = require('express');
 const path = require('path');
+
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist/angular-hon'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/dist\angular-hon/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-hon/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
