987aaf97b394ed794fb20ae49b561b28372071de
Vkv333 <aha010h@gmail.com>
2025-05-15 23:54:40 +0530

    updated server.js1

--- AngularHON/server.js
+++ AngularHON/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/angularHON'));
+app.use(express.static(__dirname + '/dist/angular-hon'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angularHON/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/dist\angular-hon/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
